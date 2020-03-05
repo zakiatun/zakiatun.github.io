274f6123399424c074ad0b568125cd0d6e68b4d6
zakiatun <zakia04.zn@gmail.com>
2020-03-06 05:30:13 +0700

    ganti tampilan

---
+++ js/display.js
@@ -0,0 +1,188 @@
+function beranda() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "none";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
+
+function semuaDoa() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (b.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "block";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "none";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
+
+function doaFavorit() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (c.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "block";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "none";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
+
+function pengulangan() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (x.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "none";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
+
+function tentang() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (y.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
+        i.style.display = "none";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
+
+function bantuan() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (z.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "block";
+        i.style.display = "none";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
+
+function infoDoa() {
+    var a = document.getElementById("beranda");
+    var b = document.getElementById("semuaDoa");
+    var c = document.getElementById("doaFavorit");
+    var x = document.getElementById("pengulangan");
+    var y = document.getElementById("tentang");
+    var z = document.getElementById("bantuan");
+    var i = document.getElementById("infoDoa");
+    if (i.style.display === "none") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    } else {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        i.style.display = "block";
+    }
+  }
