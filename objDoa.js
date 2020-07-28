10121e2f2fdf57a9b0d1d45093af0045ef0e410d
zakiatun <zakia04.zn@gmail.com>
2020-07-28 20:42:15 +0700

    fix design

---
+++ objDoa.js
@@ -0,0 +1,41 @@
+playlistHarian = [{
+    judul : 'Doa Mohon Ampun dan Rahmat Allah 1',
+    arti  : 'Ya Rabbku, sesungguhnya aku berlindung kepada-Mu untuk memohon kepada-Mu sesuatu yang aku tidak mengetahui (hakikatnya). Kalau Engkau tidak mengampuniku, dan (tidak) menaruh belas kasihan kepadaku, niscaya aku termasuk orang yang rugi.',
+    sanad : 'QS. Hud [11]: 47',
+    arab  : '001.png',
+    mp3   : '001.m4a',
+    latex : '001.txt',
+    pdf   : '001.pdf'
+},{
+    judul : 'Doa supaya Dilapangkan Hati serta Dimudahkan Urusan 1',
+    arti  : 'Tidak ada ilah selain Engkau, Mahasuci Engkau. Sungguh, aku termasuk orang-orang yang zhalim.',
+    sanad : 'QS. Al-Anbiyaa [21]: 87',
+    arab  : '030.png',
+    mp3   : '030.m4a',
+    latex : '030.txt',
+    pdf   : '030.pdf'
+},{
+    judul : 'Doa Memohon Kebaikan Dunia dan Akhirat',
+    arti  : 'Ya Rabb kami, berilah kami kebaikan di dunia dan kebaikan di akhirat, dan lindungilah kami dari azab Neraka.',
+    sanad : 'QS. Al-Baqarah [2]: 201',
+    arab  : '033.png',
+    mp3   : '033.m4a',
+    latex : '033.txt',
+    pdf   : '033.pdf'
+},{
+    judul : 'Doa supaya Hati Ditetapkan dalam Hidayah',
+    arti  : 'Ya Rabb kami, janganlah Engkau condongkan hati kami kepada kesesatan setelah Engkau berikan petunjuk kepada kami, dan karuniakanlah kepada kami rahmat dari sisi-Mu, dan sesungguhnya Engkau Maha Pemberi.',
+    sanad : 'QS. Ali Imran [3]: 8',
+    arab  : '034.png',
+    mp3   : '034.m4a',
+    latex : '034.txt',
+    pdf   : '034.pdf'
+},{
+    judul : 'Doa bagi Keamanan Negeri dan Berlindung dari Syirik',
+    arti  : 'Ya Rabb, jadikanlah negeri ini, negeri yang aman, dan jauhkanlah aku beserta anak cucuku agar tidak menyembah berhala.',
+    sanad : 'QS. Ibrahim [14]: 35',
+    arab  : '035.png',
+    mp3   : '035.m4a',
+    latex : '035.txt',
+    pdf   : '035.pdf'
+}]
---
+++ script.js
@@ -0,0 +1,215 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav        = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+const btnplaylist      = document.querySelector('.playlist');
+const btnPengulangan   = document.querySelector('.settings');
+const tutup            = document.querySelector('.close');
+const btnMore          = document.querySelector('.more');
+const tutupPengulangan = document.querySelector('.pengulangan .close');
+const popupPlaylist    = document.querySelector('.popup-doa-harian');
+const popupPengulangan = document.querySelector('.popup-pengulangan');
+const popupMore        = document.querySelector('.popup-more');
+
+
+btnplaylist.addEventListener('click', function(){
+    popupPlaylist.style.display = 'block';
+});
+
+tutup.addEventListener('click', function(){
+    popupPlaylist.style.display = 'none';
+});
+
+btnPengulangan.addEventListener('click', function(){
+    popupPengulangan.style.display = 'block';
+});
+
+tutupPengulangan.addEventListener('click', function(){
+    popupPengulangan.style.display = 'none';
+});
+
+btnMore.addEventListener('click', function(){
+    if(popupMore.style.display === 'block'){
+        popupMore.style.display = 'none';
+    }
+    else{
+        popupMore.style.display = 'block';
+    }
+});
+
+let indexHarian = 0;
+let suara       = new Audio();
+suara.src       = 'audio-doa/' + playlistHarian[indexHarian].mp3;
+suara.loop      = false;
+suara.pause();
+
+const judul  = document.querySelector('.judul-doa');
+const arab   = document.querySelector('.gambar-doa img');
+const arti   = document.querySelector('#arti');
+const sanad  = document.querySelector('#sanad');
+const latex  = document.querySelector('.latex-doa');
+const pdf    = document.querySelector('.pdf-doa');
+
+judul.innerHTML = playlistHarian[indexHarian].judul;
+arab.src        = 'img-doa/' + playlistHarian[indexHarian].arab;
+arti.innerHTML  = playlistHarian[indexHarian].arti;
+sanad.innerHTML = playlistHarian[indexHarian].sanad;
+latex.src       = 'latex-doa/' + playlistHarian[indexHarian].latex;
+pdf.src         = 'pdf-doa/' + playlistHarian[indexHarian].pdf;
+
+latex.addEventListener('click', function(){
+    window.open(latex.src);
+})
+
+pdf.addEventListener('click', function(){
+    window.open(pdf.src);
+})
+
+const btnPlay  = document.querySelector('.play');
+const btnPause = document.querySelector('.pause');
+
+const btnPlayPause = document.querySelector('.play-pause');
+btnPlayPause.addEventListener('click', function(){
+    if(suara.paused){
+        suara.play();
+        btnPlay.style.display  = 'none';
+        btnPause.style.display = 'block';
+    } else{
+        suara.pause();
+        btnPlay.style.display  = 'block';
+        btnPause.style.display = 'none';
+    }
+});
+
+const slider = document.querySelector('.slider');
+let geser    = true;
+
+slider.addEventListener('mouseup', function(e){
+    setPosisiSuara(e);
+});
+
+function setPosisiSuara(e){
+    if(geser){
+        let posisiSuara   = suara.duration * (slider.value/100);
+        suara.currentTime = posisiSuara;
+    }
+}
+
+const waktuSekarang = document.querySelector('.waktu-sekarang');
+const durasi        = document.querySelector('.durasi');
+
+suara.addEventListener('timeupdate', function(){
+    updateWaktu();
+});
+
+function updateWaktu(){
+    let waktuBaru = suara.currentTime * (100 / suara.duration);
+    slider.value  = waktuBaru;
+
+    let menitSekarang = Math.floor(suara.currentTime/60);
+    let detikSekarang = Math.floor(suara.currentTime - menitSekarang * 60);
+    let menitDurasi   = Math.floor(suara.duration/60);
+    let detikDurasi   = Math.floor(suara.duration - menitDurasi * 60);
+
+    if(menitSekarang<10){menitSekarang = "0"+menitSekarang}
+    if(detikSekarang<10){detikSekarang = "0"+detikSekarang}
+    if(menitDurasi<10){menitDurasi     = "0"+menitDurasi}
+    if(detikSekarang<10){detikSekarang = "0"+detikSekarang}
+
+    waktuSekarang.innerHTML = menitSekarang + ":" + detikSekarang;
+    durasi.innerHTML        = menitDurasi + ":" + detikDurasi;
+}
+
+suara.addEventListener('ended', function(){
+    gantiTrack();
+});
+
+function gantiTrack(){
+    if(indexHarian == playlistHarian.length - 1){
+        indexHarian = 0;
+    } else{
+        indexHarian++;
+    }
+
+    suara.src       = 'audio-doa/' + playlistHarian[indexHarian].mp3;
+    suara.play();
+
+    judul.innerHTML = playlistHarian[indexHarian].judul;
+    arab.src        = 'img-doa/' + playlistHarian[indexHarian].arab;
+    arti.innerHTML  = playlistHarian[indexHarian].arti;
+    sanad.innerHTML = playlistHarian[indexHarian].sanad;
+    latex.src       = 'latex-doa/' + playlistHarian[indexHarian].latex;
+    pdf.src         = 'pdf-doa/' + playlistHarian[indexHarian].pdf;
+
+    latex.addEventListener('click', function(){
+        window.open(latex.src);
+    })
+
+    pdf.addEventListener('click', function(){
+        window.open(pdf.src);
+    })
+}
+
+const next = document.querySelector('.next');
+next.addEventListener('click', function(){
+    indexHarian++;
+    if(indexHarian > playlistHarian.length - 1){
+        indexHarian = 0;
+    };
+
+    btnPlay.style.display  = 'none';
+    btnPause.style.display = 'block';
+
+    suara.src       = 'audio-doa/' + playlistHarian[indexHarian].mp3;
+    suara.play();
+
+    judul.innerHTML = playlistHarian[indexHarian].judul;
+    arab.src        = 'img-doa/' + playlistHarian[indexHarian].arab;
+    arti.innerHTML  = playlistHarian[indexHarian].arti;
+    sanad.innerHTML = playlistHarian[indexHarian].sanad;
+    latex.src       = 'latex-doa/' + playlistHarian[indexHarian].latex;
+    pdf.src         = 'pdf-doa/' + playlistHarian[indexHarian].pdf;
+
+    latex.addEventListener('click', function(){
+        window.open(latex.src);
+    })
+
+    pdf.addEventListener('click', function(){
+        window.open(pdf.src);
+    })
+});
+
+const previous = document.querySelector('.previous');
+previous.addEventListener('click', function(){
+    indexHarian--;
+    if(indexHarian < 0){
+        indexHarian = playlistHarian.length - 1;
+    };
+
+    btnPlay.style.display  = 'none';
+    btnPause.style.display = 'block';
+
+    suara.src       = 'audio-doa/' + playlistHarian[indexHarian].mp3;
+    suara.play();
+
+    judul.innerHTML = playlistHarian[indexHarian].judul;
+    arab.src        = 'img-doa/' + playlistHarian[indexHarian].arab;
+    arti.innerHTML  = playlistHarian[indexHarian].arti;
+    sanad.innerHTML = playlistHarian[indexHarian].sanad;
+    latex.src       = 'latex-doa/' + playlistHarian[indexHarian].latex;
+    pdf.src         = 'pdf-doa/' + playlistHarian[indexHarian].pdf;
+
+    latex.addEventListener('click', function(){
+        window.open(latex.src);
+    })
+
+    pdf.addEventListener('click', function(){
+        window.open(pdf.src);
+    })
+})
+
+
+
