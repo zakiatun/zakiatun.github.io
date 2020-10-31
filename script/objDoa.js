9998a5f2205f4873b0f9d6e31eddaa9518f0dbce
zakiatun <zakia04.zn@gmail.com>
2020-10-31 23:44:39 +0700

    bismillah

---
+++ script/objDoa.js
@@ -0,0 +1,377 @@
+objHarian = [{
+    id    : 0,
+    judul : 'Doa Mohon Ampun dan Rahmat Allah 1',
+    arti  : 'Ya Rabbku, sesungguhnya aku berlindung kepada-Mu untuk memohon kepada-Mu sesuatu yang aku tidak mengetahui (hakikatnya). Kalau Engkau tidak mengampuniku, dan (tidak) menaruh belas kasihan kepadaku, niscaya aku termasuk orang yang rugi.',
+    sanad : 'QS. Hud [11]: 47',
+    arab  : '001.png',
+    mp3   : '001.mp3',
+    latex : '001.txt',
+    pdf   : '001.pdf',
+    favorit : false
+},{
+    id    : 1,
+    judul : 'Doa supaya Dilapangkan Hati serta Dimudahkan Urusan 1',
+    arti  : 'Tidak ada ilah selain Engkau, Mahasuci Engkau. Sungguh, aku termasuk orang-orang yang zhalim.',
+    sanad : 'QS. Al-Anbiyaa [21]: 87',
+    arab  : '030.png',
+    mp3   : '030.mp3',
+    latex : '030.txt',
+    pdf   : '030.pdf',
+    favorit : false
+},{
+    id    : 2,
+    judul : 'Doa Memohon Kebaikan Dunia dan Akhirat',
+    arti  : 'Ya Rabb kami, berilah kami kebaikan di dunia dan kebaikan di akhirat, dan lindungilah kami dari azab Neraka.',
+    sanad : 'QS. Al-Baqarah [2]: 201',
+    arab  : '033.png',
+    mp3   : '033.mp3',
+    latex : '033.txt',
+    pdf   : '033.pdf',
+    favorit : false
+},{
+    id    : 3,
+    judul : 'Doa supaya Hati Ditetapkan dalam Hidayah',
+    arti  : 'Ya Rabb kami, janganlah Engkau condongkan hati kami kepada kesesatan setelah Engkau berikan petunjuk kepada kami, dan karuniakanlah kepada kami rahmat dari sisi-Mu, dan sesungguhnya Engkau Maha Pemberi.',
+    sanad : 'QS. Ali Imran [3]: 8',
+    arab  : '034.png',
+    mp3   : '034.mp3',
+    latex : '034.txt',
+    pdf   : '034.pdf',
+    favorit : false
+},{
+    id    : 4,
+    judul : 'Doa bagi Keamanan Negeri dan Berlindung dari Syirik',
+    arti  : 'Ya Rabb, jadikanlah negeri ini, negeri yang aman, dan jauhkanlah aku beserta anak cucuku agar tidak menyembah berhala.',
+    sanad : 'QS. Ibrahim [14]: 35',
+    arab  : '035.png',
+    mp3   : '035.mp3',
+    latex : '035.txt',
+    pdf   : '035.pdf',
+    favorit : false
+},{
+    id    : 5,
+    judul : 'Doa Berlindung dari Neraka',
+    arti  : "'Ya Rabb kami, jauhkanlah azab Jahanam dari kami, karena sesungguhnya azabnya itu membuat kebinasaan yang kekal,' sungguh, Jahanam itu seburuk-buruk tempat menetap dan tempat kediaman.",
+    sanad : 'QS. Al-Furqan [25]: 65-66',
+    arab  : '036.png',
+    mp3   : '036.mp3',
+    latex : '036.txt',
+    pdf   : '036.pdf',
+    favorit : false
+},{
+    id    : 6,
+    judul : 'Doa Berlindung dari Syaitan',
+    arti  : "Ya Rabbku, aku berlindung kepada Engkau dari bisikan-bisikan syaitan, dan aku berlindung (pula) kepada Engkau ya Rabbku, agar mereka tidak mendekati aku.",
+    sanad : "QS. Al-Mu'minuun [23]: 97-98",
+    arab  : '037.png',
+    mp3   : '037.mp3',
+    latex : '037.txt',
+    pdf   : '037.pdf',
+    favorit : false
+},{
+    id    : 7,
+    judul : 'Doa Berlindung dari Fitnah Kemenangan Orang Kafir 1',
+    arti  : "Ya Rabb kami, janganlah Engkau jadikan kami (sasaran) fitnah bagi orang-orang kafir. Dan ampunilah kami, ya Rabb kami. Sesungguhnya Engkau yang Mahaperkasa, Mahabijaksana.",
+    sanad : "QS. Al-Mumtahanah [60]: 5",
+    arab  : '038.png',
+    mp3   : '038.mp3',
+    latex : '038.txt',
+    pdf   : '038.pdf',
+    favorit : false
+},{
+    id    : 8,
+    judul : 'Doa Meminta Pertolongan dan Perlindungan Dari Orang Kafir',
+    arti  : "Cukuplah Allah (menjadi penolong) bagi kami dan Dia sebaik-baik pelindung.",
+    sanad : "QS. Ali 'Imran [3]: 173",
+    arab  : '040.png',
+    mp3   : '040.mp3',
+    latex : '040.txt',
+    pdf   : '040.pdf',
+    favorit : false
+},{
+    id    : 9,
+    judul : 'Doa Mohon Kesabaran dan Diwafatkan Dalam Keadaan Muslim',
+    arti  : "Ya Rabb kami, limpahkan kesabaran kepada kami dan matikanlah kami dalam keadaan muslim (berserah diri kepada-Mu).",
+    sanad : "QS. Al-A'raaf [7]: 126",
+    arab  : '041.png',
+    mp3   : '041.mp3',
+    latex : '041.txt',
+    pdf   : '041.pdf',
+    favorit : false
+},{
+    id    : 10,
+    judul : 'Doa Tasyahud 1',
+    arti  : "Semua kesejahteraan, kerajaan, dan kekekalan segala yang diberkahi; semua doa untuk mengagungkan Allah; dan seluruh perkataan yang baik dan amal shalih hanyalah milik Allah. Semoga kesejahteraan, rahmat, dan karunia Allah tercurah untukmu wahai Nabi wahai Nabi. Semoga kesejahteraan diberikan kepada kami dan hamba-hamba Allah yang shalih. Aku bersaksi bahwa tidak ada ilah yang berhak diibadahi dengan benar selain Allah dan aku bersaksi bahwasanya Muhammad adalah utusan Allah.",
+    sanad : "Shahih: HR. Muslim (no. 403 [60]), Abu Awanah (II/228), dari Abdullah bin Abbas; bahwasanya ia berkata: 'Rasulullah SAW. mengajarkan kepada kami tasyahud sebagaimana mengajarkan surah dari al-Qur-an.'",
+    arab  : '098.png',
+    mp3   : '098.mp3',
+    latex : '098.txt',
+    pdf   : '098.pdf',
+    favorit : false
+},{
+    id    : 11,
+    judul : 'Doa Menghadapi Musuh dan Orang yang Berkuasa 1',
+    arti  : "Ya Allah, sungguh kami menjadikan Engkau di leher mereka (agar kekuatan mereka tidak berdaya saat berhadapan dengan kami). Dan kami berlindung kepada-Mu dari kejelekan mereka.",
+    sanad : "Shahih: HR. Abu Dawud (no. 1537), an-Nasai dalam 'Amalul Yaum wal Lailah (no. 606), dan al-Hakim (II/142). Dishahihkan oleh al-Hakim dan disepakati oleh adz-Dzahabi.",
+    arab  : '237.png',
+    mp3   : '237.mp3',
+    latex : '237.txt',
+    pdf   : '237.pdf',
+    favorit : false
+},{
+    id    : 12,
+    judul : 'Doa supaya Dilapangkan Hati serta Dimudahkan Urusan 1',
+    arti  : 'Tidak ada ilah selain Engkau, Mahasuci Engkau. Sungguh, aku termasuk orang-orang yang zhalim.',
+    sanad : 'QS. Al-Anbiyaa [21]: 87',
+    arab  : '030.png',
+    mp3   : '030.mp3',
+    latex : '030.txt',
+    pdf   : '030.pdf',
+    favorit : false
+},{
+    id    : 13,
+    judul : 'Doa Memohon Kebaikan Dunia dan Akhirat',
+    arti  : 'Ya Rabb kami, berilah kami kebaikan di dunia dan kebaikan di akhirat, dan lindungilah kami dari azab Neraka.',
+    sanad : 'QS. Al-Baqarah [2]: 201',
+    arab  : '033.png',
+    mp3   : '033.mp3',
+    latex : '033.txt',
+    pdf   : '033.pdf',
+    favorit : false
+},{
+    id    : 14,
+    judul : 'Doa supaya Hati Ditetapkan dalam Hidayah',
+    arti  : 'Ya Rabb kami, janganlah Engkau condongkan hati kami kepada kesesatan setelah Engkau berikan petunjuk kepada kami, dan karuniakanlah kepada kami rahmat dari sisi-Mu, dan sesungguhnya Engkau Maha Pemberi.',
+    sanad : 'QS. Ali Imran [3]: 8',
+    arab  : '034.png',
+    mp3   : '034.mp3',
+    latex : '034.txt',
+    pdf   : '034.pdf',
+    favorit : false
+},{
+    id    : 15,
+    judul : 'Doa bagi Keamanan Negeri dan Berlindung dari Syirik',
+    arti  : 'Ya Rabb, jadikanlah negeri ini, negeri yang aman, dan jauhkanlah aku beserta anak cucuku agar tidak menyembah berhala.',
+    sanad : 'QS. Ibrahim [14]: 35',
+    arab  : '035.png',
+    mp3   : '035.mp3',
+    latex : '035.txt',
+    pdf   : '035.pdf',
+    favorit : false
+},{
+    id    : 16,
+    judul : 'Doa Berlindung dari Neraka',
+    arti  : "'Ya Rabb kami, jauhkanlah azab Jahanam dari kami, karena sesungguhnya azabnya itu membuat kebinasaan yang kekal,' sungguh, Jahanam itu seburuk-buruk tempat menetap dan tempat kediaman.",
+    sanad : 'QS. Al-Furqan [25]: 65-66',
+    arab  : '036.png',
+    mp3   : '036.mp3',
+    latex : '036.txt',
+    pdf   : '036.pdf',
+    favorit : false
+},{
+    id    : 17,
+    judul : 'Doa Berlindung dari Syaitan',
+    arti  : "Ya Rabbku, aku berlindung kepada Engkau dari bisikan-bisikan syaitan, dan aku berlindung (pula) kepada Engkau ya Rabbku, agar mereka tidak mendekati aku.",
+    sanad : "QS. Al-Mu'minuun [23]: 97-98",
+    arab  : '037.png',
+    mp3   : '037.mp3',
+    latex : '037.txt',
+    pdf   : '037.pdf',
+    favorit : false
+},{
+    id    : 18,
+    judul : 'Doa Berlindung dari Fitnah Kemenangan Orang Kafir 1',
+    arti  : "Ya Rabb kami, janganlah Engkau jadikan kami (sasaran) fitnah bagi orang-orang kafir. Dan ampunilah kami, ya Rabb kami. Sesungguhnya Engkau yang Mahaperkasa, Mahabijaksana.",
+    sanad : "QS. Al-Mumtahanah [60]: 5",
+    arab  : '038.png',
+    mp3   : '038.mp3',
+    latex : '038.txt',
+    pdf   : '038.pdf',
+    favorit : false
+},{
+    id    : 19,
+    judul : 'Doa Meminta Pertolongan dan Perlindungan Dari Orang Kafir',
+    arti  : "Cukuplah Allah (menjadi penolong) bagi kami dan Dia sebaik-baik pelindung.",
+    sanad : "QS. Ali 'Imran [3]: 173",
+    arab  : '040.png',
+    mp3   : '040.mp3',
+    latex : '040.txt',
+    pdf   : '040.pdf',
+    favorit : false
+},{
+    id    : 20,
+    judul : 'Doa Mohon Kesabaran dan Diwafatkan Dalam Keadaan Muslim',
+    arti  : "Ya Rabb kami, limpahkan kesabaran kepada kami dan matikanlah kami dalam keadaan muslim (berserah diri kepada-Mu).",
+    sanad : "QS. Al-A'raaf [7]: 126",
+    arab  : '041.png',
+    mp3   : '041.mp3',
+    latex : '041.txt',
+    pdf   : '041.pdf',
+    favorit : false
+},{
+    id    : 21,
+    judul : 'Doa Tasyahud 1',
+    arti  : "Semua kesejahteraan, kerajaan, dan kekekalan segala yang diberkahi; semua doa untuk mengagungkan Allah; dan seluruh perkataan yang baik dan amal shalih hanyalah milik Allah. Semoga kesejahteraan, rahmat, dan karunia Allah tercurah untukmu wahai Nabi wahai Nabi. Semoga kesejahteraan diberikan kepada kami dan hamba-hamba Allah yang shalih. Aku bersaksi bahwa tidak ada ilah yang berhak diibadahi dengan benar selain Allah dan aku bersaksi bahwasanya Muhammad adalah utusan Allah.",
+    sanad : "Shahih: HR. Muslim (no. 403 [60]), Abu Awanah (II/228), dari Abdullah bin Abbas; bahwasanya ia berkata: 'Rasulullah SAW. mengajarkan kepada kami tasyahud sebagaimana mengajarkan surah dari al-Qur-an.'",
+    arab  : '098.png',
+    mp3   : '098.mp3',
+    latex : '098.txt',
+    pdf   : '098.pdf',
+    favorit : false
+},{
+    id    : 22,
+    judul : 'Doa Menghadapi Musuh dan Orang yang Berkuasa 1',
+    arti  : "Ya Allah, sungguh kami menjadikan Engkau di leher mereka (agar kekuatan mereka tidak berdaya saat berhadapan dengan kami). Dan kami berlindung kepada-Mu dari kejelekan mereka.",
+    sanad : "Shahih: HR. Abu Dawud (no. 1537), an-Nasai dalam 'Amalul Yaum wal Lailah (no. 606), dan al-Hakim (II/142). Dishahihkan oleh al-Hakim dan disepakati oleh adz-Dzahabi.",
+    arab  : '237.png',
+    mp3   : '237.mp3',
+    latex : '237.txt',
+    pdf   : '237.pdf',
+    favorit : false
+},{
+    id    : 23,
+    judul : 'Doa supaya Dilapangkan Hati serta Dimudahkan Urusan 1',
+    arti  : 'Tidak ada ilah selain Engkau, Mahasuci Engkau. Sungguh, aku termasuk orang-orang yang zhalim.',
+    sanad : 'QS. Al-Anbiyaa [21]: 87',
+    arab  : '030.png',
+    mp3   : '030.mp3',
+    latex : '030.txt',
+    pdf   : '030.pdf',
+    favorit : false
+},{
+    id    : 24,
+    judul : 'Doa Memohon Kebaikan Dunia dan Akhirat',
+    arti  : 'Ya Rabb kami, berilah kami kebaikan di dunia dan kebaikan di akhirat, dan lindungilah kami dari azab Neraka.',
+    sanad : 'QS. Al-Baqarah [2]: 201',
+    arab  : '033.png',
+    mp3   : '033.mp3',
+    latex : '033.txt',
+    pdf   : '033.pdf',
+    favorit : false
+},{
+    id    : 25,
+    judul : 'Doa supaya Hati Ditetapkan dalam Hidayah',
+    arti  : 'Ya Rabb kami, janganlah Engkau condongkan hati kami kepada kesesatan setelah Engkau berikan petunjuk kepada kami, dan karuniakanlah kepada kami rahmat dari sisi-Mu, dan sesungguhnya Engkau Maha Pemberi.',
+    sanad : 'QS. Ali Imran [3]: 8',
+    arab  : '034.png',
+    mp3   : '034.mp3',
+    latex : '034.txt',
+    pdf   : '034.pdf',
+    favorit : false
+},{
+    id    : 26,
+    judul : 'Doa bagi Keamanan Negeri dan Berlindung dari Syirik',
+    arti  : 'Ya Rabb, jadikanlah negeri ini, negeri yang aman, dan jauhkanlah aku beserta anak cucuku agar tidak menyembah berhala.',
+    sanad : 'QS. Ibrahim [14]: 35',
+    arab  : '035.png',
+    mp3   : '035.mp3',
+    latex : '035.txt',
+    pdf   : '035.pdf',
+    favorit : false
+},{
+    id    : 27,
+    judul : 'Doa Berlindung dari Neraka',
+    arti  : "'Ya Rabb kami, jauhkanlah azab Jahanam dari kami, karena sesungguhnya azabnya itu membuat kebinasaan yang kekal,' sungguh, Jahanam itu seburuk-buruk tempat menetap dan tempat kediaman.",
+    sanad : 'QS. Al-Furqan [25]: 65-66',
+    arab  : '036.png',
+    mp3   : '036.mp3',
+    latex : '036.txt',
+    pdf   : '036.pdf',
+    favorit : false
+},{
+    id    : 28,
+    judul : 'Doa Berlindung dari Syaitan',
+    arti  : "Ya Rabbku, aku berlindung kepada Engkau dari bisikan-bisikan syaitan, dan aku berlindung (pula) kepada Engkau ya Rabbku, agar mereka tidak mendekati aku.",
+    sanad : "QS. Al-Mu'minuun [23]: 97-98",
+    arab  : '037.png',
+    mp3   : '037.mp3',
+    latex : '037.txt',
+    pdf   : '037.pdf',
+    favorit : false
+},{
+    id    : 29,
+    judul : 'Doa Berlindung dari Fitnah Kemenangan Orang Kafir 1',
+    arti  : "Ya Rabb kami, janganlah Engkau jadikan kami (sasaran) fitnah bagi orang-orang kafir. Dan ampunilah kami, ya Rabb kami. Sesungguhnya Engkau yang Mahaperkasa, Mahabijaksana.",
+    sanad : "QS. Al-Mumtahanah [60]: 5",
+    arab  : '038.png',
+    mp3   : '038.mp3',
+    latex : '038.txt',
+    pdf   : '038.pdf',
+    favorit : false
+},{
+    id    : 30,
+    judul : 'Doa Meminta Pertolongan dan Perlindungan Dari Orang Kafir',
+    arti  : "Cukuplah Allah (menjadi penolong) bagi kami dan Dia sebaik-baik pelindung.",
+    sanad : "QS. Ali 'Imran [3]: 173",
+    arab  : '040.png',
+    mp3   : '040.mp3',
+    latex : '040.txt',
+    pdf   : '040.pdf',
+    favorit : false
+},{
+    id    : 31,
+    judul : 'Doa Mohon Kesabaran dan Diwafatkan Dalam Keadaan Muslim',
+    arti  : "Ya Rabb kami, limpahkan kesabaran kepada kami dan matikanlah kami dalam keadaan muslim (berserah diri kepada-Mu).",
+    sanad : "QS. Al-A'raaf [7]: 126",
+    arab  : '041.png',
+    mp3   : '041.mp3',
+    latex : '041.txt',
+    pdf   : '041.pdf',
+    favorit : false
+},{
+    id    : 32,
+    judul : 'Doa Tasyahud 1',
+    arti  : "Semua kesejahteraan, kerajaan, dan kekekalan segala yang diberkahi; semua doa untuk mengagungkan Allah; dan seluruh perkataan yang baik dan amal shalih hanyalah milik Allah. Semoga kesejahteraan, rahmat, dan karunia Allah tercurah untukmu wahai Nabi wahai Nabi. Semoga kesejahteraan diberikan kepada kami dan hamba-hamba Allah yang shalih. Aku bersaksi bahwa tidak ada ilah yang berhak diibadahi dengan benar selain Allah dan aku bersaksi bahwasanya Muhammad adalah utusan Allah.",
+    sanad : "Shahih: HR. Muslim (no. 403 [60]), Abu Awanah (II/228), dari Abdullah bin Abbas; bahwasanya ia berkata: 'Rasulullah SAW. mengajarkan kepada kami tasyahud sebagaimana mengajarkan surah dari al-Qur-an.'",
+    arab  : '098.png',
+    mp3   : '098.mp3',
+    latex : '098.txt',
+    pdf   : '098.pdf',
+    favorit : false
+},{
+    id    : 33,
+    judul : 'Doa Menghadapi Musuh dan Orang yang Berkuasa 1',
+    arti  : "Ya Allah, sungguh kami menjadikan Engkau di leher mereka (agar kekuatan mereka tidak berdaya saat berhadapan dengan kami). Dan kami berlindung kepada-Mu dari kejelekan mereka.",
+    sanad : "Shahih: HR. Abu Dawud (no. 1537), an-Nasai dalam 'Amalul Yaum wal Lailah (no. 606), dan al-Hakim (II/142). Dishahihkan oleh al-Hakim dan disepakati oleh adz-Dzahabi.",
+    arab  : '237.png',
+    mp3   : '237.mp3',
+    latex : '237.txt',
+    pdf   : '237.pdf',
+    favorit : false
+}]
+
+if(localStorage.getItem('objFavorit') == null){
+    localStorage.setItem('objFavorit','[]')
+}
+
+let objFavorit = JSON.parse(localStorage.getItem('objFavorit'));
+
+const kosong = document.querySelector('.kosong');
+const konten = document.querySelector('.konten')
+
+if(localStorage.getItem('objFavorit') == '[]'){
+    kosong.style.display = 'block';
+    konten.style.display = 'none';
+}
+
+if(!localStorage.getItem('objHarian')){
+    localStorage.setItem('objHarian', JSON.stringify(objHarian));
+}
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav        = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+function doaHarian(){
+    window.open('../doaHarian.html', '_self')
+}
+
+const menuPlaylist = document.querySelector('.btn-playlist');
+const playlist     = document.querySelector('.playlist');
+
+menuPlaylist.addEventListener('click', function(){
+    playlist.classList.toggle('slide');
+});
---
+++ script/scriptDoaFavorit.js
@@ -0,0 +1,321 @@
+const judulDoa = document.querySelector('.judul-doa');
+const arabDoa   = document.querySelector('.arab-doa img');
+const artiDoa   = document.querySelector('.arti-doa');
+const sanadDoa  = document.querySelector('.sanad-doa');
+
+let indexDoa = 0;
+let suara    = new Audio();
+suara.loop   = false;
+
+suara.pause();
+suara.src          = '../suara/' + objFavorit[indexDoa].mp3;
+judulDoa.innerHTML = objFavorit[indexDoa].judul;
+arabDoa.src        = '../arab/' + objFavorit[indexDoa].arab;
+artiDoa.innerHTML  = objFavorit[indexDoa].arti;
+sanadDoa.innerHTML = objFavorit[indexDoa].sanad;
+
+function infoDoa(index){
+    suara.src          = '../suara/' + objFavorit[index].mp3;
+    judulDoa.innerHTML = objFavorit[index].judul;
+    arabDoa.src        = '../arab/' + objFavorit[index].arab;
+    artiDoa.innerHTML  = objFavorit[index].arti;
+    sanadDoa.innerHTML = objFavorit[index].sanad;
+    pP();
+    suara.play();
+}
+
+suara.onended = function(){
+  tanpaPengulangan();
+};
+
+const slider = document.querySelector('.slider');
+let geser    = true;
+
+slider.addEventListener('mouseup', function(e){
+    setPosisiSuara(e);
+});
+
+function setPosisiSuara(e){
+    if(geser){
+        let posisiSuara   = suara.duration * (slider.value/100);
+        suara.currentTime = posisiSuara;
+    }
+}
+
+const waktuSekarang = document.querySelector('.waktu');
+const durasi        = document.querySelector('.durasi');
+
+suara.addEventListener('timeupdate', function(){
+    updateWaktu();
+});
+
+function updateWaktu(){
+    let waktuBaru = suara.currentTime * (100 / suara.duration);
+    slider.value  = waktuBaru;
+
+    let menitSekarang = Math.floor(suara.currentTime/60);
+    let detikSekarang = Math.floor(suara.currentTime - menitSekarang * 60);
+    let menitDurasi   = Math.floor(suara.duration/60);
+    let detikDurasi   = Math.floor(suara.duration - menitDurasi * 60);
+
+    if(menitSekarang<10){menitSekarang = "0"+menitSekarang}
+    if(detikSekarang<10){detikSekarang = "0"+detikSekarang}
+    if(menitDurasi<10){menitDurasi     = "0"+menitDurasi}
+    if(detikDurasi<10){detikDurasi     = "0"+detikDurasi}
+
+    waktuSekarang.innerHTML = menitSekarang + ":" + detikSekarang;
+    durasi.innerHTML        = menitDurasi + ":" + detikDurasi;
+}
+
+const btnPrevious  = document.querySelector('.previous');
+const playPause    = document.querySelector('.play-pause');
+const btnPlay      = document.querySelector('.play');
+const btnPause     = document.querySelector('.pause');
+const btnNext      = document.querySelector('.next');
+
+btnPrevious.addEventListener('click', function(){
+    let indexLama = indexDoa;
+    indexDoa--;
+    if(indexDoa < 0 ){
+        indexDoa = objFavorit.length - 1;
+    };
+    updateStylePlaylist(indexLama, indexDoa)
+    infoDoa(indexDoa);
+});
+
+btnNext.addEventListener('click', function(){
+    let indexLama = indexDoa
+    indexDoa++;
+    if(indexDoa > objFavorit.length - 1){
+        indexDoa = 0;
+    };
+    updateStylePlaylist(indexLama, indexDoa)
+    infoDoa(indexDoa);
+});
+
+playPause.addEventListener('click', pP);
+
+function pP(){
+    if(suara.paused){
+        suara.play();
+        btnPause.style.display = 'block';
+        btnPlay.style.display  = 'none';
+        document.querySelector('#doa-'+indexDoa).classList.add("mainkan");
+        playToPause(indexDoa)
+    }else{
+        suara.pause();
+        btnPause.style.display = 'none';
+        btnPlay.style.display  = 'block';
+        pauseToPlay(indexDoa)
+    }
+}
+
+function updateStylePlaylist(indexLama, indexBaru){
+    document.querySelector('#doa-'+indexLama).classList.remove("mainkan");
+    pauseToPlay(indexLama);
+    document.querySelector('#doa-'+indexBaru).classList.add("mainkan");
+    playToPause(indexBaru)
+  }
+
+function playToPause(indexDoa){
+    let ele = document.querySelector('#pp-'+indexDoa)
+    ele.classList.remove("pp-play");
+    ele.classList.add("pp-pause");
+}
+
+function pauseToPlay(indexDoa){
+    let ele = document.querySelector('#pp-'+indexDoa)
+    ele.classList.remove("pp-pause");
+    ele.classList.add("pp-play");
+}
+
+const pengulangan = document.querySelector('.repeating');
+const btnTanpa    = document.querySelector('.none');
+const btnSatu     = document.querySelector('.one');
+const btnSemua    = document.querySelector('.all');
+
+btnTanpa.addEventListener('click', function(){
+  btnTanpa.style.display = 'none';
+  btnSatu.style.display  = 'block';
+  btnSemua.style.display = 'none';
+
+  suara.loop = true;
+});
+
+btnSatu.addEventListener('click', function(){
+  btnTanpa.style.display = 'none';
+  btnSatu.style.display  = 'none';
+  btnSemua.style.display = 'block';
+
+  suara.loop  = false;
+  suara.onended = function(){
+      ulangiSemua();
+  }
+});
+
+btnSemua.addEventListener('click', function(){
+  btnTanpa.style.display = 'block';
+  btnSatu.style.display  = 'none';
+  btnSemua.style.display = 'none';
+
+  suara.loop    = false;
+  suara.onended = function(){
+      tanpaPengulangan();
+  }
+});
+
+function ulangiSemua(){
+    let indexLama = indexDoa;
+    if(indexDoa >= objFavorit.length - 1){
+        indexDoa = 0;
+    } else{
+        indexDoa++;
+    }
+    updateStylePlaylist(indexLama, indexDoa)
+    infoDoa(indexDoa);
+}
+
+function tanpaPengulangan(){
+    let indexLama = indexDoa;
+    if(indexDoa == objFavorit.length - 1){
+        btnPause.style.display = 'none';
+        btnPlay.style.display  = 'block';
+    }
+    indexDoa++;
+    updateStylePlaylist(indexLama, indexDoa)
+    infoDoa(indexDoa);
+}
+
+function itemPlaylist(index, judul, pdf, latex){
+    const track = document.createElement('div');
+    track.setAttribute('class','track');
+    track.setAttribute('id', 'doa-' + index)
+    document.querySelector('.playlist-favorit').appendChild(track);
+
+    const btnPlayPause = document.createElement('button');
+    btnPlayPause.setAttribute('class', 'pp pp-play');
+    btnPlayPause.setAttribute('id', 'pp-' + index);
+    btnPlayPause.setAttribute('data-index', index);
+    document.querySelector('#doa-' + index).appendChild(btnPlayPause);
+
+    const judulTrack = document.createElement('span');
+    judulTrack.innerHTML = judul;
+    document.querySelector('#doa-' + index).appendChild(judulTrack);
+
+    const btnMore = document.createElement('button');
+    btnMore.setAttribute('class', 'btn-more');
+    btnMore.setAttribute('id', 'btn-more-' + index);
+    document.querySelector('#doa-' + index).appendChild(btnMore);
+
+    const div = document.createElement('div');
+    div.setAttribute('class', 'more');
+    div.setAttribute('id', 'more-' + index);
+    document.querySelector('#btn-more-' + index).appendChild(div)
+
+    const hapusTrack = document.createElement('button');
+    hapusTrack.setAttribute('class', 'hapus');
+    hapusTrack.setAttribute('data-index', index);
+    hapusTrack.innerHTML = 'Hapus Doa';
+    document.querySelector('#more-' + index).appendChild(hapusTrack); 
+
+    const btnPdf = document.createElement('button');
+    btnPdf.setAttribute('class', 'pdf');
+    btnPdf.setAttribute('data-index', index);
+    btnPdf.innerHTML = 'File Pdf';
+    btnPdf.src = '../pdf/' + pdf;
+    document.querySelector('#more-' + index).appendChild(btnPdf);
+
+    const btnLatex = document.createElement('button');
+    btnLatex.setAttribute('class', 'latex');
+    btnLatex.setAttribute('data-index', index);
+    btnLatex.innerHTML = 'Kode Latex';
+    btnLatex.src = '../latex/' + latex;
+    document.querySelector('#more-' + index).appendChild(btnLatex);
+}
+
+for(let i = 0; i < objFavorit.length; i++){
+   itemPlaylist(i, objFavorit[i].judul), objFavorit[i].pdf, objFavorit[i].latex;
+}
+
+const tTrack = document.querySelectorAll('.pp')
+
+for(let i = 0; i < tTrack.length; i++){
+    tTrack[i].addEventListener('click', gantiTrack.bind(this));
+}
+
+function gantiTrack(event){
+  for(let i = 0; i < tTrack.length; i++){
+      if(tTrack[i] == event.target){
+          let indexTrack = event.target.getAttribute('data-index');
+          if(indexTrack == indexDoa){
+              pP();
+          } else{ 
+            indexDoa = parseInt(indexTrack);
+            infoDoa(indexDoa);
+          }
+      } else{
+        tTrack[i].classList.remove('pp-pause');
+        tTrack[i].classList.add('pp-play');
+        tTrack[i].parentElement.classList.remove('mainkan');
+      }
+  }
+}
+
+const lihatPdf = document.querySelectorAll('.pdf');
+
+for(let i = 0; i < lihatPdf.length; i++){
+    lihatPdf[i].addEventListener('click', function(){
+        window.open('../pdf/'+objFavorit[i].pdf);
+    });
+}
+
+const lihatLatex = document.querySelectorAll('.latex');
+
+for(let i = 0; i < lihatLatex.length; i++){
+    lihatLatex[i].addEventListener('click', function(){
+        window.open('../latex/'+objFavorit[i].latex);
+    });
+}
+
+const listTrackBaru = document.querySelector('.playlist-favorit');
+
+listTrackBaru.addEventListener('click', function(e){
+    let data      = localStorage.getItem('objHarian');
+    let objHarian = JSON.parse(data);
+
+    let objFavorit = JSON.parse(localStorage.getItem('objFavorit'));
+    
+    let targetDoa = e.target;
+
+    if(targetDoa.classList[0] === 'hapus'){
+        let idTarget = targetDoa.getAttribute('data-index');
+        for(keys in objFavorit[idTarget]){
+            if(keys == 'favorit' && objFavorit[idTarget][keys] == true){
+                let cek = objHarian.find((item) => item.id === objFavorit[idTarget].id);
+                if (cek){
+                    let hapus = confirm('Kamu yakin ingin menghapus doa ini?');
+                    if(hapus === true){
+                        cek.favorit = false;
+                        objFavorit.splice(idTarget, 1);
+                        targetDoa.parentElement.style.display = 'none';
+                        location.reload()
+                    }
+                } 
+            }
+        }
+    localStorage.setItem('objFavorit', JSON.stringify(objFavorit));
+    localStorage.setItem('objHarian', JSON.stringify(objHarian));
+    }
+})
+
+const cari = document.querySelector('.cari');
+const track = document.querySelectorAll('.track');
+cari.addEventListener('keyup', function(){
+    Array.prototype.forEach.call(track, function(e){
+        if(e.textContent.toLowerCase().indexOf(cari.value) > -1){
+            e.style.display = 'flex';
+        } else {
+            e.style.display = 'none';
+        }
+    });
+});
